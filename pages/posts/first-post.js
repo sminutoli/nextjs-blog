f72b98a4a0d7af871a2268479181d1773714d981
Seryo's macbook <seryosmacbook@Seryoss-MacBook-Pro.local>
2023-02-01 14:57:27 -0300

    Add global CSS. Use module.css files. Enrich Layout component. Use meta:og and <Image> with priority

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,12 +1,11 @@
-import Link from "next/link";
 import Head from "next/head";
+import Layout from "../../components/Layout";
 
 export default function FirstPost() {
-    return <>
+    return <Layout>
         <Head>
             <title>My First Post</title>
         </Head>
         <h1>First Postre</h1>
-        <Link href="/">Back to home</Link>
-    </>;
+    </Layout>;
 }
