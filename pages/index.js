9264f855dcc6e6a0f002f3b86c9316f8a720cfdf
Seryo's macbook <seryosmacbook@Seryoss-MacBook-Pro.local>
2023-02-02 14:37:59 -0300

    First static content

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,36 @@
 import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
+import Layout, {siteTitle} from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import {getSortedPostsData} from "../lib/posts";
 
-export default function Home() {
-  return (
-      <Layout home>
-        <Head>
-          <title>{siteTitle}</title>
-        </Head>
-        <section className={utilStyles.headingMd}>
-          <p>Hola Pipis, vengo a flotar</p>
-        </section>
-      </Layout>
-  );
+export default function Home({allPostsData}) {
+  return (<Layout home>
+    <Head>
+      <title>{siteTitle}</title>
+    </Head>
+    <section className={utilStyles.headingMd}>
+      <p>Hola Pipis, vengo a flotar</p>
+    </section>
+
+    <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      <h2 className={utilStyles.headingLg}>Blog</h2>
+      <ul className={utilStyles.list}>
+        {allPostsData.map(({id, date, title}) => (<li className={utilStyles.listItem} key={id}>
+          <h3>{title}</h3>
+          <p>{id}</p>
+          <p>{date}</p>
+        </li>))}
+      </ul>
+    </section>
+
+  </Layout>);
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
